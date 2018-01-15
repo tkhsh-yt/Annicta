67ed2cd23e4b8f6b0b16b1dd2582bbe29dd7a536
takahashi <takahashi@ipl.cs.uec.ac.jp>
2018-01-16 00:24:21 +0900

    Add UserConfig
    本当はもっと前にステージングしないといけなかったファイル

---
+++ app/src/main/java/tkhshyt/annicta/pref/UserConfig.kt
@@ -0,0 +1,8 @@
+package tkhshyt.annicta.pref
+
+import com.chibatching.kotpref.KotprefModel
+
+object UserConfig : KotprefModel() {
+    var shareTwitter by booleanPref(false)
+    var shareFacebook by booleanPref(false)
+}
