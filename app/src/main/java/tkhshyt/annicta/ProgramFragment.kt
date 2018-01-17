9159e470edb9a191ddcb08839e5049523b4f0583
takahashi <takahashi@ipl.cs.uec.ac.jp>
2018-01-17 22:28:48 +0900

    Add load more items

--- app/src/main/java/tkhshyt/annicta/ProgramFragment.kt
+++ app/src/main/java/tkhshyt/annicta/ProgramFragment.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import com.mikepenz.fastadapter.FastAdapter
 import com.mikepenz.fastadapter.adapters.ItemAdapter
+import com.mikepenz.fastadapter_extensions.scroll.EndlessRecyclerOnScrollListener
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_list.*
@@ -23,7 +24,9 @@ import tkhshyt.annicta.utils.Utils
 
 class ProgramFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
 
-    val programItemAdapter = ItemAdapter<ProgramItem>()
+    private val programItemAdapter = ItemAdapter<ProgramItem>()
+    private var loading = false
+    private var ended = false
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_list, container, false)
@@ -39,6 +42,39 @@ class ProgramFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
         recyclerView.setHasFixedSize(true)
         recyclerView.layoutManager = llm
 
+        recyclerView.addOnScrollListener(object : EndlessRecyclerOnScrollListener() {
+            override fun onLoadMore(currentPage: Int) {
+                if(!loading && !ended) {
+                    val accessToken = UserInfo.accessToken
+                    if(accessToken != null) {
+                        if(programItemAdapter.adapterItemCount != 0) {
+                            val last = programItemAdapter.getAdapterItem(programItemAdapter.adapterItemCount-1)
+                            val startedAt = Utils.apiDateFormat.format(last.program.started_at)
+
+                            AnnictClient.service.programs(
+                                    access_token = accessToken,
+                                    sort_started_at = "desc",
+                                    filter_started_at_lt = startedAt
+                            ).subscribeOn(Schedulers.io())
+                                .observeOn(AndroidSchedulers.mainThread())
+                                .subscribe({ programs ->
+                                    if(programs.programs.size != 1) {
+                                        (1 until programs.programs.size).forEach {
+                                            programItemAdapter.add(programItemAdapter.adapterItemCount, ProgramItem(programs.programs[it], context))
+                                        }
+                                        loading = false
+                                    } else {
+                                        ended = true
+                                    }
+                                }, { throwable: Throwable? ->
+                                    Toast.makeText(context, "読み込みに失敗しました", Toast.LENGTH_LONG).show()
+                                })
+                        }
+                    }
+                }
+            }
+        })
+
         swipeRefreshView.setOnRefreshListener(this)
         swipeRefreshView.setColorSchemeResources(R.color.greenPrimary, R.color.redPrimary, R.color.indigoPrimary, R.color.yellowPrimary)
 
@@ -51,8 +87,6 @@ class ProgramFragment : Fragment(), SwipeRefreshLayout.OnRefreshListener {
     }
 
     private fun updatePrograms() {
-        val adapter = recyclerView.adapter
-
         val accessToken = UserInfo.accessToken
         if (accessToken != null) {
             val head =
