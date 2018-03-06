8453456645059e0bb39a6a821270193b9c625ace
takahashi <takahashi@ipl.cs.uec.ac.jp>
2018-03-07 02:04:57 +0900

    Remove unnecessary extention

--- app/src/main/java/tkhshyt/annicta/utils/KotlinUtil.kt
+++ app/src/main/java/tkhshyt/annicta/utils/KotlinUtil.kt
@@ -1,15 +1,8 @@
 package tkhshyt.annicta.utils
 
-fun <A, B> A?.notNull(f: (A) -> B?): B? =
-        if (this != null) {
-            f(this)
-        } else {
-            null
-        }
-
 fun <A, B> A?.notNullIf(p: (A) -> Boolean, f: (A) -> B?): B? =
         if (this != null && p(this)) {
-            this.notNull(f)
+            f(this)
         } else {
             null
         }
