61221c01c2b353e102c4a829ebbb2d364c902e01
takahashi <takahashi@ipl.cs.uec.ac.jp>
2018-03-07 22:31:33 +0900

    Fix

--- app/src/main/java/tkhshyt/annicta/ActivityItem.kt
+++ app/src/main/java/tkhshyt/annicta/ActivityItem.kt
@@ -49,26 +49,27 @@ class ActivityItem(val activity: Activity, val act: android.app.Activity?) : Abs
 
             val activity = item?.activity
             val work = activity?.work
-            if (activity != null && act != null) {
+
+            if (activity != null && act != null && work != null) {
                 val linkColor = ContextCompat.getColor(act, R.color.light_blue_800)
                 when (activity.action) {
                     Action.CreateRecord -> {
                         val text = String.format("%s が %s %s を見ました",
                                 AndroidUtil.colorHtml(activity.user?.name.orEmpty(), linkColor),
-                                AndroidUtil.colorHtml(activity.work?.title.orEmpty(), linkColor),
+                                AndroidUtil.colorHtml(activity.work.title.orEmpty(), linkColor),
                                 AndroidUtil.colorHtml(activity.episode?.number_text.orEmpty(), linkColor))
                         itemView.activity.text = AndroidUtil.fromHtml(text)
                     }
                     Action.CreateReview -> {
                         val text = String.format("%s が %s のレビューを書きました ",
                                 AndroidUtil.colorHtml(activity.user?.name.orEmpty(), linkColor),
-                                AndroidUtil.colorHtml(work?.title.orEmpty(), linkColor))
+                                AndroidUtil.colorHtml(work.title.orEmpty(), linkColor))
                         itemView.activity.text = AndroidUtil.fromHtml(text)
                     }
                     Action.CreateMultipleRecords -> {
                         var text = String.format("%s が %s ",
                                 activity.user?.name.orEmpty(),
-                                work?.title.orEmpty())
+                                work.title.orEmpty())
                         val records = activity.multiple_record
                         text += if (records?.size == 1) {
                             records.first().episode.number_text.orEmpty()
@@ -83,82 +84,82 @@ class ActivityItem(val activity: Activity, val act: android.app.Activity?) : Abs
                     Action.CreateStatus -> {
                         val text = String.format("%s が %s を「%s」に変更しました",
                                 AndroidUtil.colorHtml(activity.user?.name.orEmpty(), linkColor),
-                                AndroidUtil.colorHtml(work?.title.orEmpty(), linkColor),
+                                AndroidUtil.colorHtml(work.title.orEmpty(), linkColor),
                                 AndroidUtil.colorHtml(AnnictUtil.kindText(activity.status?.kind), linkColor))
                         itemView.activity.text = AndroidUtil.fromHtml(text)
                     }
                 }
-            }
 
-            itemView.workCard.setOnClickListener {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    act?.go(
-                            Page.WORK,
-                            ActivityOptionsCompat.makeSceneTransitionAnimation(act, itemView.workIcon, itemView.workIcon.transitionName).toBundle(),
-                            { it.putExtra("work_id", work?.id) }
-                    )
-                } else {
-                    act?.go(Page.WORK, { it.putExtra("work_id", work?.id) })
+                itemView.workCard.setOnClickListener {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        act.go(
+                                Page.WORK,
+                                ActivityOptionsCompat.makeSceneTransitionAnimation(act, itemView.workIcon, itemView.workIcon.transitionName).toBundle(),
+                                { it.putExtra("work_id", work.id) }
+                        )
+                    } else {
+                        act.go(Page.WORK, { it.putExtra("work_id", work.id) })
+                    }
                 }
-            }
 
-            var imageUrl: String? = null
-            if (work?.images?.recommended_url != null && work.images.recommended_url.isNotBlank()) {
-                imageUrl = work.images.recommended_url
-            } else if (work?.images?.twitter?.image_url != null && work.images.twitter.image_url.isNotBlank()) {
-                imageUrl = work.images.twitter.image_url
-            }
-            if (imageUrl != null) {
-                Glide.with(this.act)
-                    .load(imageUrl)
-                    .diskCacheStrategy(DiskCacheStrategy.ALL)
-                    .into(itemView.workIcon)
-            }
+                var imageUrl: String? = null
+                if (work.images?.recommended_url != null && work.images.recommended_url.isNotBlank()) {
+                    imageUrl = work.images.recommended_url
+                } else if (work.images?.twitter?.image_url != null && work.images.twitter.image_url.isNotBlank()) {
+                    imageUrl = work.images.twitter.image_url
+                }
+                if (imageUrl != null) {
+                    Glide.with(this.act)
+                        .load(imageUrl)
+                        .diskCacheStrategy(DiskCacheStrategy.ALL)
+                        .into(itemView.workIcon)
+                }
 
-            itemView.workTitle.text = work?.title
-            var selectedItem = -1
-            val adapter = object : ArrayAdapter<String>(this.act, R.layout.item_status, this.act?.resources?.getStringArray(R.array.work_status_array)) {
+                itemView.workTitle.text = work.title
+                var selectedItem = -1
+                val adapter = object : ArrayAdapter<String>(this.act, R.layout.item_status, this.act.resources?.getStringArray(R.array.work_status_array)) {
 
-                override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-                    val view: View?
-                    if (convertView == null) {
-                        view = (context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater)
-                            .inflate(R.layout.item_status, null)
-                        view.findViewById<TextView>(R.id.status)?.text = getItem(position)
+                    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+                        val view: View?
+                        if (convertView == null) {
+                            view = (context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater)
+                                .inflate(R.layout.item_status, null)
+                            view.findViewById<TextView>(R.id.status)?.text = getItem(position)
+                            return view
+                        }
+                        convertView.findViewById<TextView>(R.id.status)?.text = getItem(position)
+
+                        return convertView
+                    }
+
+                    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View {
+                        val view = super.getDropDownView(position, convertView, parent)
+                        if (position == selectedItem) {
+                            view.setBackgroundColor(ContextCompat.getColor(context, R.color.blue_700))
+                        } else {
+                            view.setBackgroundColor(ContextCompat.getColor(context, R.color.grey_800))
+                        }
                         return view
                     }
-                    convertView.findViewById<TextView>(R.id.status)?.text = getItem(position)
-
-                    return convertView
                 }
+                adapter.setDropDownViewResource(R.layout.item_status_dropdown)
 
-                override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View {
-                    val view = super.getDropDownView(position, convertView, parent)
-                    if (position == selectedItem) {
-                        view.setBackgroundColor(ContextCompat.getColor(context, R.color.blue_700))
-                    } else {
-                        view.setBackgroundColor(ContextCompat.getColor(context, R.color.grey_800))
+                val selection = Math.max(Kind.stringToIndex(work.status?.kind), 0)
+                itemView.statusSpinner.adapter = adapter
+                itemView.statusSpinner.setSelection(selection)
+
+                itemView.statusSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+
+                    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                        if (selectedItem != -1 && selectedItem != position) {
+                            EventBus.getDefault().post(UpdateStatusEvent(work.id
+                                    ?: -1, Kind.values()[position].kind))
+                        }
+                        selectedItem = position
                     }
-                    return view
-                }
-            }
-            adapter.setDropDownViewResource(R.layout.item_status_dropdown)
 
-            val selection = Math.max(Kind.stringToIndex(work?.status?.kind), 0)
-            itemView.statusSpinner.adapter = adapter
-            itemView.statusSpinner.setSelection(selection)
-
-            itemView.statusSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-
-                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                    if (selectedItem != -1 && selectedItem != position) {
-                        EventBus.getDefault().post(UpdateStatusEvent(work?.id
-                                ?: -1, Kind.values()[position].kind))
+                    override fun onNothingSelected(parent: AdapterView<*>?) {
                     }
-                    selectedItem = position
-                }
-
-                override fun onNothingSelected(parent: AdapterView<*>?) {
                 }
             }
         }
