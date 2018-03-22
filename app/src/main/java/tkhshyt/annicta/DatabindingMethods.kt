171accda3be826b3787cd9e57fd3906aa7dc7ae2
takahashi <takahashi@ipl.cs.uec.ac.jp>
2018-03-23 02:59:41 +0900

    Fix bug with BindingAdapter

--- app/src/main/java/tkhshyt/annicta/DatabindingMethods.kt
+++ app/src/main/java/tkhshyt/annicta/DatabindingMethods.kt
@@ -1,45 +1,43 @@
 package tkhshyt.annicta
 
 import android.databinding.BindingAdapter
-import android.support.v4.content.res.ResourcesCompat
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
-import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.broadcast_bar.view.*
 import tkhshyt.annicta.util.AnnictUtil
 import java.util.*
 
-object DatabindingMethods {
+@BindingAdapter(value = ["bind:imageUrl", "bind:placeholderId", "bind:centerCrop", "bind:circleCrop"], requireAll = false)
+fun ImageView.setImageUrl(imageUrl: String, placeholderId: Int? = null, centerCrop: Boolean = false, circleCrop: Boolean = false) {
+    if(imageUrl.isNotEmpty()) {
 
-    @JvmStatic
-    @BindingAdapter("bind:date")
-    fun setDate(textView: TextView, date: Date) {
-        textView.text = AnnictUtil.textDateFormat.format(date)
-    }
+        var options = RequestOptions()
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
 
-    @JvmStatic
-    @BindingAdapter(value = ["bind:imageUrl", "bind:placeholderId", "bind:centerCrop", "bind:circleCrop"], requireAll = false)
-    fun setImageUrl(imageView: ImageView, imageUrl: String, placeholderId: Int, centerCrop: Boolean?, circleCrop: Boolean?) {
-        if(imageUrl.isNotEmpty()) {
-
-            var options = RequestOptions()
-                .placeholder(placeholderId)
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-
-            if(centerCrop == true) {
-                options = options.centerCrop()
-            }
-
-            if(circleCrop == true) {
-                options = options.circleCrop()
-            }
-
-            Glide.with(imageView.context)
-                .load(imageUrl)
-                .apply(options)
-                .into(imageView)
+        if (placeholderId != null) {
+            options = options.placeholder(placeholderId)
         }
+
+        if(centerCrop) {
+            options = options.centerCrop()
+        }
+
+        if(circleCrop) {
+            options = options.circleCrop()
+        }
+
+        Glide.with(this.context)
+            .load(imageUrl)
+            .apply(options)
+            .into(this)
     }
+}
+
+@BindingAdapter("bind:date")
+fun TextView.setDate(date: Date) {
+    this.text = AnnictUtil.textDateFormat.format(date)
 }
